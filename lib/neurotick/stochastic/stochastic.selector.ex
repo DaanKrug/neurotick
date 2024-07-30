37c316d8af6f9b0af9d6798c2a54b2e4bef5ae62
Daniel Augusto Krug <daniel.krug@fepese.org.br>
2024-07-30 08:36:02 -0300

    feat: adding stochastic PI rand weight perturbation function

--- lib/neurotick/stochastic/stochastic.selector.ex
+++ lib/neurotick/stochastic/stochastic.selector.ex
@@ -3,13 +3,19 @@ defmodule Neurotick.Stochastic.Selector do
   #alias Krug.NumberUtil
   alias Neurotick.Probability.Selector
   
+  @meta_pi 314159
+  @meta_pi_range (314159 * 2) + 1
+  @meta_pi_divider 10000
+  
   def select_elements(elements) do
     probability = 1 / (elements |> length() |> :math.sqrt())
     Selector.select(elements,probability)
   end
   
+  # -PI to + PI
   def choose_weight_perturbation() do
-    # -PI to + PI
+    rand = :rand.uniform(@meta_pi_range) - 1 - @meta_pi
+    rand / @meta_pi_divider
   end
   
   def max_attemps(elements) do
